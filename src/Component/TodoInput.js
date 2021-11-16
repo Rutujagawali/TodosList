cf2af35a92d9f042a63da091478f61692e5c5a72
Rutujagawali <86350670+Rutujagawali@users.noreply.github.com>
2021-11-16 17:35:50 +0530

    React_todolist

---
+++ src/Component/TodoInput.js
@@ -0,0 +1,45 @@
+import './TodoInput.css';
+//import styles from "./TodoItem.module.css"
+
+import React, { useState } from 'react';
+const TodoInput = (props) => {
+    const [inputText,setInputText] = useState({
+      title: "",
+    })
+    const onChange = e => {
+        setInputText({
+          ...inputText,
+          [e.target.name]: e.target.value,
+        })
+      }
+    
+      const handleSubmit = e => {
+        e.preventDefault();
+        
+        if(inputText.title.trim()) {
+          props.addTodoProps(inputText.title);
+          setInputText({
+            title: "",
+          })
+        } else {
+          alert("Please write item")
+        } 
+      };
+    
+return(
+
+
+<form  onSubmit={handleSubmit} className="form-container">
+  <input
+    type="text"
+    className="input-text"
+    placeholder="Add todo..."
+    value={inputText.title}
+    name="title"
+    onChange={onChange}
+  />
+  <button className="input-submit">Submit</button>
+</form>
+  )
+}
+export default TodoInput;
